4e7bbda2b71b44aac1eab48bbc23b62d94f9f2a0
Ridel Oscar García Mora <ridel.garcia@ntsprint.com>
2022-01-16 10:38:03 -0500

    adding files to repository

---
+++ src/main/java/com/drones/dronesapi/DronesapiApplication.java
@@ -0,0 +1,13 @@
+package com.drones.dronesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesapiApplication.class, args);
+	}
+
+}
