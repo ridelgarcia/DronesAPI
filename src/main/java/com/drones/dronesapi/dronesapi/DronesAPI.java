3acc3b51a954189938dba70546c6cd485b2a87d8
Ridel Oscar García Mora <ridel.garcia@ntsprint.com>
2022-01-16 12:13:10 -0500

    fixing error to map controller routes

--- src/main/java/com/drones/dronesapi/dronesapi/DronesAPI.java
+++ src/main/java/com/drones/dronesapi/dronesapi/DronesAPI.java
@@ -1,4 +1,4 @@
-package com.drones.dronesapi;
+package com.drones.dronesapi.dronesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
